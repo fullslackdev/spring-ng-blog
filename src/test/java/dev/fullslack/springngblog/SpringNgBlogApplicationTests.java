61deb55b00a42869822b399100d6eef42b8fe7fc
Bart L <loeffen@hotmail.com>
2021-01-17 22:50:00 +0100

    Initial commit
    Part 3 of the tutorial completed.

---
+++ src/test/java/dev/fullslack/springngblog/SpringNgBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fullslack.springngblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNgBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
