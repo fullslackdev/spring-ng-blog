4ec1197d01173ea9e646a5042415906e9369dffd
Bart L <loeffen@hotmail.com>
2021-01-18 16:56:33 +0100

    Fix errors in part 10

--- src/main/java/dev/fullslack/springngblog/controller/AuthController.java
+++ src/main/java/dev/fullslack/springngblog/controller/AuthController.java
@@ -3,6 +3,7 @@ package dev.fullslack.springngblog.controller;
 import dev.fullslack.springngblog.dto.LoginRequest;
 import dev.fullslack.springngblog.dto.RegisterRequest;
 import dev.fullslack.springngblog.service.AuthService;
+import dev.fullslack.springngblog.service.AuthenticationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
     }
 }
